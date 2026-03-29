7569356ce6bfe703ddf1e9e7b080c33393c66ed5
Nikos Stylianou <68811067+nikosstyl@users.noreply.github.com>
2026-03-29 20:52:14 +0200

    Fixed compilation error

--- Core/Inc/flash_utils.h
+++ Core/Inc/flash_utils.h
@@ -2,6 +2,7 @@
 #define __FLASH_UTILS__
 
 #include <stdint.h>
+#include "stm32f3xx_hal.h"
 
 uint8_t Flash_Read  (uint32_t addr, void *buffer, uint32_t size);
 HAL_StatusTypeDef Flash_Write (uint32_t addr, const void *data, uint32_t size);
